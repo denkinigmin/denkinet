9ea45e594f960fbc845e3a4b5842b7a609758fa0
denkinigmin <167005179+denkinigmin@users.noreply.github.com>
2024-04-14 22:39:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+
+window.addEventListener("load",()=>{
+    for (const bar of document.getElementsByClassName("cf")){
+        for(const a of bar.getElementsByTagName("a")){
+            if(a.href==window.location.href){
+                a.className="active";
+            }
+        }
+    }
+}
+);
+
