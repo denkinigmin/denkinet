a93ac9ca5c262cae15f8e8ac0915288edc532b72
denkinigmin <167005179+denkinigmin@users.noreply.github.com>
2024-04-15 10:46:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-
-
-window.addEventListener("load",()=>{
-    for (const bar of document.getElementsByClassName("cf")){
-        for(const a of bar.getElementsByTagName("a")){
-            if(a.href==window.location.href){
-                a.className="active";
+"use scrict";
+$(function(){
+    const $header=$('#header');
+    window.addEventListener("load",()=>{
+        for (const bar of document.getElementsByClassName("cf")){
+            for(const a of bar.getElementsByTagName("a")){
+                if(a.href==window.location.href){
+                        a.className="active";
+                    }
+                }
             }
         }
-    }
-}
-);
+    );
+    window.addEventListener("scroll", () => {
+    // windowのスクロール量を取得する
+    const scrollTop = $(window).scrollTop();
 
+    // ヘッダーの高さが60pxなので、この値を基準にする
+    if (scrollTop > 60) {
+      // hideクラスがない場合は、hideを追加する
+      if (!$header.hasClass("hide")) {
+        $header.addClass("hide");
+      }
+    } else {
+      // hideクラスがある場合は、hideを削除する
+      if ($header.hasClass("hide")) {
+        $header.removeClass("hide");
+      }
+    }
+  });
+    
+});
