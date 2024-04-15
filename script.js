7435b0a51c70fbf1befa9d676309372c4c6611f0
denkinigmin <167005179+denkinigmin@users.noreply.github.com>
2024-04-15 10:47:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,22 +11,6 @@ $(function(){
             }
         }
     );
-    window.addEventListener("scroll", () => {
-    // windowのスクロール量を取得する
-    const scrollTop = $(window).scrollTop();
-
-    // ヘッダーの高さが60pxなので、この値を基準にする
-    if (scrollTop > 60) {
-      // hideクラスがない場合は、hideを追加する
-      if (!$header.hasClass("hide")) {
-        $header.addClass("hide");
-      }
-    } else {
-      // hideクラスがある場合は、hideを削除する
-      if ($header.hasClass("hide")) {
-        $header.removeClass("hide");
-      }
-    }
-  });
+    
     
 });
